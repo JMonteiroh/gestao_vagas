7df5277ddd26e38b1aa6bdf635bad3442866133b
João Paulo <joao.paulomonteiro@hotmail.com>
2024-01-07 22:49:45 -0300

    Creating candidate token

---
+++ src/main/java/br/com/joaomonteiro/gestao_vagas/modules/candidate/dto/AuthCandidateResquestDTO.java
@@ -0,0 +1,5 @@
+package br.com.joaomonteiro.gestao_vagas.modules.candidate.dto;
+
+public record AuthCandidateResquestDTO(String username, String password) {
+    
+}
--- src/main/java/br/com/joaomonteiro/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
+++ src/main/java/br/com/joaomonteiro/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
@@ -9,4 +9,5 @@ import br.com.joaomonteiro.gestao_vagas.modules.candidate.entities.CandidateEnti
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
     Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+    Optional<CandidateEntity> findByUsername(String username);
 }
